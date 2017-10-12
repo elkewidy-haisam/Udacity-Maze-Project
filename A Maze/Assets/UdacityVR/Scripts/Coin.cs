1ab112b6d634fd79950fa4f9cd7398d9e7d198ed
Haisam <elkewidyhaisam@gmail.com>
2017-10-12 09:36:04 -0400

    maze works; keys work; doors unlock; coins collected

--- A Maze/Assets/UdacityVR/Scripts/Coin.cs
+++ A Maze/Assets/UdacityVR/Scripts/Coin.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour 
 {
+    public GameObject CoinPoof;
+    public GameObject coin;
+    
+
     //Create a reference to the CoinPoofPrefab
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
+
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+        DestroyGameObject();
     }
 
+    void DestroyGameObject()
+    {
+
+        Destroy(gameObject);
+
+    }
 }
--- A Maze/Assets/UdacityVR/Scripts/Key.cs
+++ A Maze/Assets/UdacityVR/Scripts/Key.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class Key : MonoBehaviour 
 {
     //Create a reference to the KeyPoofPrefab and Door
+    public GameObject key;
+    public Door door;
 
-	void Update()
+    private bool keyCollected;
+
+    void Update()
 	{
 		//Not required, but for fun why not try adding a Key Floating Animation here :)
 	}
 
 	public void OnKeyClicked()
 	{
-        // Instatiate the KeyPoof Prefab where this key is located
+        // Instantiate the KeyPoof Prefab where this key is located
+
         // Make sure the poof animates vertically
+        //Object.Instantiate(keyPoof, transform.position, Quaternion.Euler(-90, 0, 0));
         // Call the Unlock() method on the Door
+        door.Unlock();
+        Debug.Log("Door should be unlocked now.");
         // Set the Key Collected Variable to true
+        keyCollected = true;
         // Destroy the key. Check the Unity documentation on how to use Destroy
+        DestroyGameObject();
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
     }
 
 }
